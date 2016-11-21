c400c1bc5f04ba319a918975acfde4ec72e9b3db
Charles <c.depoortere28@gmail.com>
2016-11-21 10:43:08 +0100

    Add fake DB in controller and display it in Index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,11 @@
 class RestaurantsController < ApplicationController
 
+  RESTAURANTS = [
+                    { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
+                    { name: "Sushi Samba", address: "City, London", category: "sushi" }
+                  ]
+
   def index
+    @restaurants = RESTAURANTS
   end
 end
